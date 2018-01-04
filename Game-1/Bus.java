5404d94a84bdd009c542d0bb63a6bce97aea2c5d
Joshua <2140748@slu.edu.ph>
2018-01-04 20:26:15 +0800

    Game Files (Codes and UI)

---
+++ Game-1/Bus.java
@@ -0,0 +1,44 @@
+package com.codeforces;
+import java.util.*;
+
+public class Bus {
+	public static void Main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int targetPoint = input.nextInt();
+		int litersGas = input.nextInt();
+		int gasStation = input.nextInt();
+		int numberOfJourneys = input.nextInt();
+		int startingPoint = litersGas;
+		startingPoint = startingPoint - gasStation;
+		gasStation = targetPoint - gasStation;
+		int answer = 0;
+		for(int i = 1; i < numberOfJourneys; i++){
+			if(startingPoint < 0){
+				System.out.println("-1");
+				return;
+			}
+			if(2 * gasStation > startingPoint){
+				startingPoint = litersGas;
+				answer++;
+			}
+			startingPoint -= 2*gasStation;
+			gasStation = targetPoint-gasStation;
+		}
+		if(startingPoint < 0){
+			System.out.println("-1");
+			return;
+		}
+		
+		if(gasStation > startingPoint){
+			startingPoint=litersGas;
+			answer++;
+		}
+		startingPoint -= gasStation;
+		if(startingPoint < 0){
+			answer =- 1;
+		}
+		System.out.println(answer);
+	
+	}
+
+}
